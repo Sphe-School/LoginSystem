d565919a9708085a266c075ef7d500efbb9ef4c6
Sphe-School <ST10536537@rcconnect.edu.za>
2026-04-13 19:26:50 +0200

    Add files via upload

---
+++ MainT1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+//ST10536537//
+public class MainT1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter last name: ");
+        String lastName = scanner.nextLine();
+
+        LoginSystem loginSystem = new LoginSystem(firstName, lastName);
+
+        System.out.print("Create username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Create password: ");
+        String password = scanner.nextLine();
+
+        System.out.print("Enter South African cell number (e.g. +27838968976): ");
+        String cellPhone = scanner.nextLine();
+
+        String registrationMessage = loginSystem.registerUser(username, password, cellPhone);
+        System.out.println(registrationMessage);
+
+        if (registrationMessage.equals("User is registered")) {
+            System.out.println("\n--- Login ---");
+
+            System.out.print("Enter username: ");
+            String enteredUsername = scanner.nextLine();
+
+            System.out.print("Enter password: ");
+            String enteredPassword = scanner.nextLine();
+
+            boolean loginSuccess = loginSystem.loginUser(enteredUsername, enteredPassword);
+
+            System.out.println(loginSystem.returnLoginStaus(loginSuccess));
+        }
+
+        scanner.close();
+    }
+}
